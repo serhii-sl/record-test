b7afd9ab1cda1aa0f875708f7a2e75ae7ef91d77
Serhii Slavita <serhii.slavita@gmail.com>
2022-03-26 22:22:27 +0200

    add full logic

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/*   video:  {
+    facingMode: {
+      exact: 'environment'
+    }} 
+  */
+
+const RESULT_IMAGE_SIZE = 300
+
 const constraints = window.constraints = {
   audio: false,
   video:  {
@@ -8,13 +16,85 @@ const constraints = window.constraints = {
     }}
 };
 
+const dataBlob = []
+
+const setVideoPreview = (video) => {
+  if (video) {
+    const canvas = document.querySelector('#canvas-preview')
+
+    const squareSize =
+      video.videoWidth > video.videoHeight
+        ? video.videoHeight
+        : video.videoWidth
+
+    const imageWidth = (video.videoWidth - squareSize) / 2
+    const imageHeight = (video.videoHeight - squareSize) / 2
+
+    canvas.width = RESULT_IMAGE_SIZE
+    canvas.height = RESULT_IMAGE_SIZE
+
+    const context = canvas.getContext('2d')
+
+    if (context) {
+      context.drawImage(
+        video,
+        imageWidth,
+        imageHeight,
+        squareSize,
+        squareSize,
+        0,
+        0,
+        RESULT_IMAGE_SIZE,
+        RESULT_IMAGE_SIZE
+      )
+    }
+  }
+}
+
+const startRecording = (stream) => {
+  const recorder = new MediaRecorder(stream)
+
+  recorder.ondataavailable = (event) => dataBlob.push(event.data)
+  recorder.start()
+
+  const stopped = new Promise((resolve, reject) => {
+    recorder.onstop = resolve
+    recorder.onerror = (event) => reject(event.type)
+  })
+
+  setTimeout(() => {
+    recorder.stop()
+  }, 6000)
+
+  return stopped.then(() => {
+    const recordedBlob = new Blob(dataBlob, { type: 'video/mp4' })
+
+    const recordedVideo = URL.createObjectURL(recordedBlob)
+    
+    const video = document.querySelector('#recorder-preview');
+    const playPreview = document.querySelector('#play-preview');
+
+    playPreview.addEventListener('click', () => video.play());
+
+    video.src = recordedVideo;
+    video.load()
+    video.currentTime = 0.5;
+    
+    video.onseeked = () => setTimeout(() => {
+      setVideoPreview(video)
+    }, 1000)
+  })
+}
+
 function handleSuccess(stream) {
-  const video = document.querySelector('video');
+  const video = document.querySelector('#gum-local');
   const videoTracks = stream.getVideoTracks();
   console.log('Got stream with constraints:', constraints);
   console.log(`Using video device: ${videoTracks[0].label}`);
-  window.stream = stream; // make variable available to browser console
+
   video.srcObject = stream;
+
+  startRecording(stream)
 }
 
 function handleError(error) {
