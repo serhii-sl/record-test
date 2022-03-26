3379fef8c05e91c81e75e5cee8b6807daefa2687
Serhii Slavita <serhii.slavita@gmail.com>
2022-03-26 20:57:32 +0200

    init 2

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-/*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
 'use strict';
 
-// Put variables in global scope to make them available to the browser console.
 const constraints = window.constraints = {
   audio: false,
   video:  {
